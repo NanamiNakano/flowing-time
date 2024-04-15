d346d9d226bd4c1c4d3ecd09524860f3b467102d
NanamiNakano <64841155+NanamiNakano@users.noreply.github.com>
2024-04-15 19:12:07 +0800

    SeoMeta

--- app.vue
+++ app.vue
@@ -1,3 +1,19 @@
 <template>
   <NuxtPage/>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: "流水の歳月",
+  meta: [
+    {name: "description", content: "I can not hear the people sing."}
+  ]
+})
+
+useSeoMeta({
+  title: "流水の歳月",
+  ogTitle: "流水の歳月",
+  description: "I can not hear the people sing.",
+  ogDescription: "I can not hear the people sing.",
+})
+</script>
