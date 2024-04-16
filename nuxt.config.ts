2ac8109a5a90aa9c30798a646b8bbaf82dffc6ec
NanamiNakano <64841155+NanamiNakano@users.noreply.github.com>
2024-04-16 21:47:43 +0800

    Remove manual installed TailwindCSS since nuxtui added it

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: {enabled: true},
-    css: ['~/assets/css/main.css', '@fortawesome/fontawesome-svg-core/styles.css'],
+    css: ['@fortawesome/fontawesome-svg-core/styles.css'],
     modules: ["@nuxt/fonts", "@nuxt/image", "@nuxt/ui"],
 
     postcss: {
