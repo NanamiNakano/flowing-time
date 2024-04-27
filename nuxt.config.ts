c2f9759ee0ea35ae56b4df7be9a0752458d98e95
NanamiNakano <64841155+NanamiNakano@users.noreply.github.com>
2024-04-27 15:52:02 +0800

    Sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,77 +1,84 @@
 export default defineNuxtConfig({
-    devtools: {enabled: true},
-    css: [
-        "~/assets/css/tailwind.css",
-        "~/assets/css/transition.css",
-        "@fortawesome/fontawesome-svg-core/styles.css"
+  devtools: { enabled: true },
+  css: [
+    '~/assets/css/tailwind.css',
+    '~/assets/css/transition.css',
+    '@fortawesome/fontawesome-svg-core/styles.css',
+  ],
+  modules: [
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxt/eslint',
+    '@nuxtjs/seo',
+    '@nuxt/content',
+    '@nuxtjs/sitemap',
+  ],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  runtimeConfig: {
+    public: {
+      site: {
+        uri: '',
+      },
+      github: {
+        appId: '',
+        appName: '',
+        clientId: '',
+      },
+    },
+    github: {
+      privateKey: '',
+      clientSecret: '',
+    },
+  },
+
+  app: {
+    layoutTransition: { name: 'layout', mode: 'out-in' },
+    pageTransition: { name: 'layout', mode: 'out-in' },
+  },
+
+  fonts: {
+    families: [
+      { name: 'Monaco', provider: 'local' },
     ],
-    modules: [
-        "@nuxt/fonts",
-        "@nuxt/image",
-        "@nuxt/ui",
-        "@nuxt/eslint",
-        "@nuxtjs/seo",
-        "@nuxt/content"
+  },
+
+  colorMode: {
+    fallback: 'light',
+  },
+
+  site: {
+    url: 'https://www.thynanami.dev',
+    name:
+            '流水の歳月',
+    description:
+            'I can not hear the people sing.',
+    defaultLocale:
+            'en',
+  },
+
+  content: {
+    highlight: {
+      theme: 'min-dark',
+    },
+  },
+
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+
+  sitemap: {
+    sources: [
+      '/api/__sitemap__/urls',
     ],
-
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {},
-        },
-    },
-
-    runtimeConfig: {
-        public: {
-            site: {
-                uri: "",
-            },
-            github: {
-                appId: "",
-                appName: "",
-                clientId: "",
-            }
-        },
-        github: {
-            privateKey: "",
-            clientSecret: "",
-        }
-    },
-
-    app: {
-        layoutTransition: {name: 'layout', mode: 'out-in'},
-        pageTransition: {name: 'layout', mode: 'out-in'},
-    },
-
-    fonts: {
-        families: [
-            {name: "Monaco", provider: "local"}
-        ]
-    },
-
-    colorMode: {
-        fallback: "light"
-    },
-
-    site: {
-        url: "https://www.thynanami.dev",
-        name:
-            "流水の歳月",
-        description:
-            "I can not hear the people sing.",
-        defaultLocale:
-            "en"
-    },
-
-    content: {
-        highlight: {
-            theme: "min-dark"
-        }
-    },
-
-    eslint: {
-        config: {
-            stylistic: true
-        }
-    }
+  },
 })
